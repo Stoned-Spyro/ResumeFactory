914059bb0bc2a5ced7ed680c5aa1f8793d44bcc8
Oleksiy Slavich <alexseeko15@gmail.com>
2020-04-13 19:36:51 +0300

    created logout button

---
+++ src/components/log_out.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class='log-out'>
+      <button @click="logout">Logout</button>
+  </div>   
+</template>
+
+<script>
+import firebase from 'firebase';
+
+
+export default {
+    name: 'logout',
+    methods:{
+        logout: function(){
+            firebase.auth().signOut().then(()=>{
+                this.$router.replace('login')
+            })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
