f4914193be47dc8f5ce3b40e1ba5008050ab8d7c
Oleksiy Slavich <alexseeko15@gmail.com>
2020-04-14 19:44:11 +0300

    changed firebase account

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,9 +1,10 @@
 export default{
-    apiKey: "AIzaSyBhqToV11fypnm86vTfhscYsFo_XpxIK1M",
-    authDomain: "resumefactory12.firebaseapp.com",
-    databaseURL: "https://resumefactory12.firebaseio.com",
-    projectId: "resumefactory12",
-    storageBucket: "resumefactory12.appspot.com",
-    messagingSenderId: "247558246897",
-    appId: "1:247558246897:web:8c5122879fa3448b3a7450"
+    apiKey: "AIzaSyBIZl9myvYhdMs6Swi8QFLU_m2jUoSlonY",
+    authDomain: "resumefactory228.firebaseapp.com",
+    databaseURL: "https://resumefactory228.firebaseio.com",
+    projectId: "resumefactory228",
+    storageBucket: "resumefactory228.appspot.com",
+    messagingSenderId: "927215984116",
+    appId: "1:927215984116:web:71dbf06678b8e0771bada1",
+    measurementId: "G-VHWEKB29WL"
 }
