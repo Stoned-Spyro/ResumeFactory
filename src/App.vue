5e1982893ab7f6ad0278df17d291c7c7475a2ace
joshbuysell <mcdrakmc@gmail.com>
2020-05-15 03:37:42 +0300

    some changeplease continue

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,14 @@
  
  
 </script>
+<style lang="scss">
 
+.card{
+ border-radius: 12px;
+ background-color:rgba(0,0,0,0.6) !important;
+ padding:10px;
+}
+</style>
 <style>
 		#app {
 		background: #38324E;
