9d43e36d56e50d0bc82801426e7e1a71207fa326
Ilya Shmelov <crosbu2002@gmail.com>
2020-05-26 15:09:34 +0300

    validation for addinfo

--- src/views/AddInfo.vue
+++ src/views/AddInfo.vue
@@ -11,49 +11,54 @@
     <section class="hero is-tiny is-white">
 		<div class="hero-body">
             <b-field label="Name">
-                <b-input placeholder="John"></b-input>
+                <b-input placeholder="John" type="text"  maxlength="15" minlength="2" required validation-message="Name must be at least 2 letters."></b-input>
             </b-field>
             <b-field label="Surname">
-                <b-input placeholder="Doe"></b-input>
+                <b-input placeholder="Doe" type="text" maxlength="15" minlength="2" required validation-message="Surname  must be at least 2 letters."></b-input>
             </b-field>
             <b-field label="Phone number">
-                <b-input placeholder="+380123456789"></b-input>
+                <b-input placeholder="Number"
+                    required
+                    type="text" minlength ="10"
+                    maxlength="20" 
+                    validation-message="Number must be at least 10 figures.">
+                </b-input>  
             </b-field>
         </div>
 	</section>
     <section class="hero is-tiny is-white">
 		<div class="hero-body">
             <b-field label="Specializing in:">
-                <b-input maxlength="200" type="textarea" placeholder="Web-design"></b-input>
+                <b-input maxlength="150" type="textarea" placeholder="Web-design," ></b-input>
             </b-field>
         </div>
 	</section>
     <section class="hero is-tiny is-white">
 		<div class="hero-body">
             <b-field label="Skills:">
-                <b-input placeholder="UI Design, Animation"></b-input>
+                <b-input placeholder="UI Design, Animation"  type="text" maxlength="50"  validation-message="This field must have at most 50 letters."></b-input>
             </b-field>
             <b-field label="Languages:">
-                <b-input placeholder="Ukrainian, English"></b-input>
+                <b-input placeholder="Ukrainian, English"  type="text" maxlength="30" minlength="4" required validation-message="This field  must be at least 4 letters and must have at most 30 letters."></b-input>
             </b-field>
             <b-field label="Platforms:">
-                <b-input placeholder=""></b-input>
+                <b-input placeholder="GitHub"  type="text" maxlength="20"   validation-message="This field must have at most 20 letters."></b-input>
             </b-field>
         </div>
 	</section>
     <section class="hero is-tiny is-white">
 		<div class="hero-body">
             <b-field label="Education:">
-                <b-input placeholder=""></b-input>
+                <b-input placeholder=""  type="text" maxlength="25"   validation-message="This field must have at most 25 letters."></b-input>
             </b-field>
             <b-field label="Institution:">
-                <b-input placeholder=""></b-input>
+                <b-input placeholder=""  type="text" maxlength="25"   validation-message="This field must have at most 25 letters."></b-input>
             </b-field>
             <b-field label="B.S:">
-                <b-input placeholder=""></b-input>
+                <b-input placeholder=""  type="text" maxlength="25"   validation-message="This field must have at most 25 letters."></b-input>
             </b-field>
             <b-field label="Minor:">
-                <b-input placeholder=""></b-input>
+                <b-input placeholder=""  type="text" maxlength="25"   validation-message="This field must have at most 25 letters."></b-input>
             </b-field>
         </div>
 	</section>
